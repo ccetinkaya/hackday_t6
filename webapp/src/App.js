d777b36eddaf57867cb12425d57643c9a30126a8
Cengiz Cetinkaya <cengiz.cetinkaya@swisscom.com>
2020-11-27 17:32:11 +0100

    Add initial frontend visitorcounter

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,45 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <p>
-          <center>
-           Quantification of visitors of cultural events in Lucerne
-          </center>
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-         <center>
-            Create a prototype of a technical solution to count visitors of cultural events
-          <p>
-            
-          </p>
-          <p>
-            
-          </p>
-          </center>
-        <a
-          className="App-link"
-          href="https://event-visitors-of-lucerne.compel.ch/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start....
-        </a>
-        <p>
 
-        </p>
-        <a
-          className="App-link"
-          href="https://hack.opendata.ch/project/653"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HackDay - Shape my City - Team 6  
-        </a>
-      </header>
+      <Button variant="primary">6030</Button>{' '}
+      <Button variant="secondary">6020</Button>{' '}
+      <Button variant="success">6032</Button>{' '}
+      <Button variant="warning">6005</Button>{' '}
     </div>
   );
 }
