0d14cc34f01acd744f999dc4183662bd48dc031a
Cengiz Cetinkaya <cengiz.cetinkaya@swisscom.com>
2020-11-27 14:41:31 +0100

    add src files

---
+++ webapp/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+          <center>
+           Quantification of visitors of cultural events in Lucerne
+          </center>
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+         <center>
+            Create a prototype of a technical solution to count visitors of cultural events
+          <p>
+            
+          </p>
+          <p>
+            
+          </p>
+          </center>
+        <a
+          className="App-link"
+          href="https://event-visitors-of-lucerne.compel.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Start....
+        </a>
+        <p>
+
+        </p>
+        <a
+          className="App-link"
+          href="https://hack.opendata.ch/project/653"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          HackDay - Shape my City - Team 6  
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
