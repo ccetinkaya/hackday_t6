da9f914be408323405da9833074344aaecdb58df
Cengiz Cetinkaya <cengiz.cetinkaya@swisscom.com>
2020-11-27 18:08:08 +0100

    Added some text lines and ln

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,7 +6,13 @@ import Button from 'react-bootstrap/Button'
 function App() {
   return (
     <div className="App">
-
+      <text>
+        <br />
+        Shape my City - 27-28. November 2020<br />
+        <br></br>
+        Quantification of visitors of cultural events in Lucerne<br />
+      </text>
+      <br></br>
       <Button variant="primary">6030</Button>{' '}
       <Button variant="secondary">6020</Button>{' '}
       <Button variant="success">6032</Button>{' '}
