d885eecf87801d247030f4f549014e9d3059260c
adrianari <adrianaricklin@gmail.com>
2020-11-28 12:06:15 +0100

    buttons added

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,16 +7,26 @@ function App() {
   return (
     <div className="App">
       <text>
-        <br />
-        Shape my City - 27-28. November 2020<br />
-        <br></br>
-        Quantification of visitors of cultural events in Lucerne<br />
+        <h3>Shape my City - 27-28. November 2020</h3>
+        <h2> Quantification of visitors of cultural events in Lucerne</h2>
+    
       </text>
       <br></br>
-      <Button variant="primary">6030</Button>{' '}
-      <Button variant="secondary">6020</Button>{' '}
-      <Button variant="success">6032</Button>{' '}
-      <Button variant="warning">6005</Button>{' '}
+      <Button variant="secondary">6002</Button>{' '}
+      <Button variant="secondary">6003</Button>{' '}
+      <Button variant="secondary">6004</Button>{' '}
+      <Button variant="secondary">6005</Button>{' '}
+	  <Button variant="secondary">6006</Button>{' '}
+	  <Button variant="secondary">6010</Button>{' '}
+	  <Button variant="secondary">6011</Button>{' '}
+	  <Button variant="secondary">6012</Button>{' '}
+	  <Button variant="secondary">6014</Button>{' '}
+	  <Button variant="secondary">6015</Button>{' '}
+	  <Button variant="secondary">6020</Button>{' '}
+	  <Button variant="secondary">6030</Button>{' '}
+	  <Button variant="secondary">6032</Button>{' '}
+	  <Button variant="secondary">6047</Button>{' '}
+	  <Button variant="secondary">6048</Button>{' '}
     </div>
   );
 }
